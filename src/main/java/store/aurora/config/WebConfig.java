24cbd36811ff7d535af56cd625b2679919ca46f0
gimchanghwan <rlackdghks8@naver.com>
2024-12-26 18:08:46 +0900

    interceptor 삭제

--- src/main/java/store/aurora/config/WebConfig.java
+++ src/main/java/store/aurora/config/WebConfig.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import store.aurora.common.argumentResolver.AuthUsernameResolver;
-import store.aurora.interceptor.CategoryInterceptor;
 
 import java.util.List;
 
@@ -18,7 +16,6 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthUsernameResolver authUsernameResolver;
-    private final CategoryInterceptor categoryInterceptor;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -34,12 +31,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter();
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(categoryInterceptor)
-                .addPathPatterns("/**");  // 필요한 URL 패턴 설정
     }
 
 }
