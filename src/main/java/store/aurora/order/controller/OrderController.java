fd3b963508560483ce9488a121fc13b105b0715f
sjh1108 <ch6854@naver.com>
2025-01-03 15:55:53 +0900

    feat: Order 프로세스 * 상세 페이지에서 주문 페이지로 이동

---
+++ src/main/java/store/aurora/order/controller/OrderController.java
@@ -0,0 +1,9 @@
+package store.aurora.order.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+}
