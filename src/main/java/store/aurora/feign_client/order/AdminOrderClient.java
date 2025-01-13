ed48f065c6b2acc7ce2697e4c347a6dcb98d8554
sjh1108 <ch6854@naver.com>
2025-01-13 16:39:18 +0900

    feat: Add Pageable to shipment management page

--- src/main/java/store/aurora/feign_client/order/AdminOrderClient.java
+++ src/main/java/store/aurora/feign_client/order/AdminOrderClient.java
@@ -1,7 +1,10 @@
 package store.aurora.feign_client.order;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import store.aurora.order.dto.AdminOrderDTO;
 
 import java.util.List;
@@ -9,5 +12,9 @@ import java.util.List;
 @FeignClient(name="adminOrderClient", url = "${api.gateway.base-url}" + "/api/admin/order")
 public interface AdminOrderClient {
     @GetMapping
-    List<AdminOrderDTO> getAllOrderList();
+    Page<AdminOrderDTO> getAllOrderList(@RequestParam("page") int page, @RequestParam("size") int size);
+
+
+//    @PostMapping("/shipment-update/{orderId}")
+//    void updateShipmentStatus(Long orderId, String shipmentState);
 }
