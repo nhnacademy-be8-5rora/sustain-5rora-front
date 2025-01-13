505dcb2b045c3c900981f363eee109c7f8ce78d3
sjh1108 <ch6854@naver.com>
2025-01-13 13:28:06 +0900

    feat: Get Order List * Order List 받는 기능 추가

--- src/main/java/store/aurora/order/controller/AdminOrderController.java
+++ src/main/java/store/aurora/order/controller/AdminOrderController.java
@@ -2,16 +2,27 @@ package store.aurora.order.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import store.aurora.feign_client.order.AdminOrderClient;
+import store.aurora.order.dto.AdminOrderDTO;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin/order")
 public class AdminOrderController {
 
+    private final AdminOrderClient adminOrderClient;
+
     @GetMapping("/shipment")
-    public String shipmentManagementPage(){
+    public String shipmentManagementPage(Model model){
+        List<AdminOrderDTO> orderList = adminOrderClient.getAllOrderList();
+
+        model.addAttribute("orderList", orderList);
+
         return "admin/order/shipment";
     }
 }
