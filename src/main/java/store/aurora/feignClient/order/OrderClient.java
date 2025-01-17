6759c24cf6a7936e78411fe276186c047aed50d1
sjh1108 <ch6854@naver.com>
2025-01-17 14:39:50 +0900

    feat: Add select wrap list when order process * 주문 과정에서 포장지 목록 불러오고, 선택할 수 있는 기능 추가

--- src/main/java/store/aurora/feignClient/order/OrderClient.java
+++
@@ -1,8 +0,0 @@
-package store.aurora.feignClient.order;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "orderClient", url = "${api.gateway.base-url}" + "/api/order")
-public interface OrderClient {
-
-}
