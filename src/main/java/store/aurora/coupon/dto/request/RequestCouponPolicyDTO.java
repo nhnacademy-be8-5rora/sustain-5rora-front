bf3d04acd254158663dc2cf6383632fa1de19a3f
adffdfewqefw <bbeekhw@gmail.com>
2025-01-23 16:46:35 +0900

    front coupon dto 경량화

--- src/main/java/store/aurora/coupon/dto/request/RequestCouponPolicyDTO.java
+++ src/main/java/store/aurora/coupon/dto/request/RequestCouponPolicyDTO.java
@@ -2,6 +2,7 @@ package store.aurora.coupon.dto.request;
 
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import store.aurora.coupon.dto.SaleType;
@@ -9,6 +10,7 @@ import store.aurora.coupon.dto.SaleType;
 //정책 쿠폰 생성 설정
 @Getter
 @Setter
+@Builder
 public class RequestCouponPolicyDTO {
     @NotNull private String policyName;
     @NotNull private SaleType saleType;
